a9cd28862d0bdab8c712eb515318535cc65f6de6
yunfengzhang.zyf <yunfengzhang.zyf@alibaba-inc.com>
2020-03-09 11:50:13 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,37 @@
 extern crate eventual;
 extern crate chrono;
 extern crate termion;
-use termion::terminal_size;
 use eventual::Timer;
 use std::sync::mpsc::Sender;
 use chrono::{NaiveTime, Utc, DateTime, Local};
-use termion::{color, style};
+use termion::{color, style, terminal_size};
+use termion::screen::*;
 use std::process::{Command};
-use std::io::{self, Write};
+use std::io::{self, Write, stdout};
 use std::time::Instant;
 
 
 fn main() {
+	let mut screen = AlternateScreen::from(stdout());
+    screen.flush().unwrap();
 	let cols =  Command::new("tput").arg("cols").output().expect("failed to exec tput cols");
 	let lines =  Command::new("tput").arg("lines").output().expect("failed to exec tpu lines");
-	println!("{:?} {:?}", cols.stdout, &lines.stdout);
+	// println!("{:?} {:?}", cols.stdout, &lines.stdout);
     let timer = Timer::new();
     let ticks = timer.interval_ms(1000).iter();
+	let size = terminal_size().unwrap();
+	// println!("{:?}", size);
+	let center_txt = String::from("Hello this is word's center");
+	let center_txt_len = center_txt.chars().count() as u16;
+	let x = size.0/2 as u16 - u16::from(center_txt_len)/2;
+	let y = size.1/2 as u16;
+	print!("{}",color::Fg(color::Red));		
+	print!("{}", style::Bold);		
     for _ in ticks {
-		print!("{}",color::Fg(color::Red));		
-		print!("{}", style::Bold);		
+		Command::new("clear").output().expect("clear error");
 		// 清除屏幕
         print!("{}[2J", 27 as char);
-		let size = terminal_size().unwrap();
-		// println!("{:?}", size);
-        print!("{}{}", termion::clear::All, termion::cursor::Goto(size.0/2 as u16, size.1 / 2 as u16));
-		println!("Hello this is word's center");
+   	    print!("{}{}", termion::clear::All, termion::cursor::Goto(x, y));
+		println!("{}", center_txt);
     }
 }
